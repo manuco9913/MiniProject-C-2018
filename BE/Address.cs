38a33673b3fc3e9f0dfac0fafebcf0e941e4f2a1
Manu <manu.co9913@gmail.com>
2018-12-28 10:32:57 +0200

    boh

--- BE/Address.cs
+++ BE/Address.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-// Aviad Upload to github first time
 namespace BE
 {
     struct Adress//struct that describes an address
