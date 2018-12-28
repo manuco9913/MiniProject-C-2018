8e342257c8a5b680d84bddd73e7590065f96a8fe
Aviad <aviadd6@gmail.com>
2018-12-28 10:36:14 +0200

    No changes

--- BE/Tester.cs
+++ BE/Tester.cs
@@ -8,14 +8,6 @@ namespace BE
 {
     public class Tester
     {
-<<<<<<< HEAD
-        public long _idPass { get; set; } // PassportID
-        public string _lastName { get; set; }// LastName
-        public string _firstName { get; set; } // FirstName
-        public string _dateOfBirth { get; set; } // Date of birth
-        public string _gender { get; set; }// gender of the examiner
-        public long _phoneNum { get; set; }//phone number of the tester
-=======
         public string _numId { get; set; }
         public string _lastName { get; set; }
         public string _firstName { get; set; }
@@ -26,7 +18,7 @@ namespace BE
         public string _experienceYears { get; set; }
         public string _maximumTestsPerWeek { get; set; }
         public string _whichCarUses { get; set; }
->>>>>>> 6937037c5e10b511597d3e9c4cefd0e8135f0447
+
 
     }
 }
