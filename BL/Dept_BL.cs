899c76318214dc0401555c15f5e5589ce9e9c567
Manu <manu.co9913@gmail.com>
2019-01-17 15:37:58 +0200

    manu

--- BL/Dept_BL.cs
+++ BL/Dept_BL.cs
@@ -119,6 +119,11 @@ namespace BL
                     throw new Exception("The trainee cannot take the test since he was tested recently");
                 if (!testerAndTraineeUseSameCarType(drivingTest.Tester_ID, drivingTest.Trainee_ID))
                     throw new Exception("Tester and trainee do not use the same type of car");
+                if (!testerMaxTestWeekly(drivingTest.Tester_ID))
+                    throw new Exception("Tester reached his maximum number of tests");
+                if (!testerAvailableTesting(drivingTest.Tester_ID))
+                    throw new Exception("The tester is not available during these hours");
+
                     
                 
 
@@ -129,6 +134,16 @@ namespace BL
                 throw e;
 
             }
+            return true;
+        }
+
+        private bool testerAvailableTesting(string tester_ID)
+        {
+            return true;
+        }
+
+        private bool testerMaxTestWeekly(string tester_ID)
+        {
             return true;
         }
 
